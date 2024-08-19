6ba4d0f462d8f5fdeeb208d0686952c2098a97f2
Kauaninhakrs <111157690+Kauaninhakrs@users.noreply.github.com>
2024-08-19 08:22:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+alternativas: [
+    {
+        texto: "Incluir estudos culturais no currículo escolar!",
+        afirmacao: [
+            "Desenvolvimento da compreensão mais profunda e respeitosa das diversas culturas ",
+        ]
+    },
+    {
+        texto: "Promover atividades extracurriculares relacionados à cultura!",
+        afirmacao: [
+            "Engajamento e motivação em sua aprendizagem, visto que e sentem mais conectados com o conteúdos",
+        ]
+    }
+]
